02d830740efc90202fdb2a29e64995552bd18899
nastia <nastia.2.r.2@gmail.com>
2025-07-29 20:56:29 +0300

    ~ clean up

--- worker/email_worker.py
+++ worker/email_worker.py
@@ -16,16 +16,19 @@ def send_email(to, subject, body):
         server.send_message(msg)
         print(f" [x] Sent email to {to}")
 
+
 def callback(ch, method, properties, body):
     message = json.loads(body)
     send_email(message['to'], message['subject'], message['body'])
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
+
 channel.queue_declare(queue='email_queue')
-channel.basic_qos(prefetch_count=1)  # не брати нові задачі, поки не завершено попередню
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='email_queue', on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
