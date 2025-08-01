e75ab59561c8ccdcf719336accaf04d76514ea4a
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2025-08-01 21:20:54 +0300

    ~ update email_worker.py

--- worker/email_worker.py
+++ worker/email_worker.py
@@ -6,6 +6,8 @@ import os
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_USER = os.getenv("EMAIL_USER")
@@ -18,7 +20,6 @@ def send_email(to, subject, body):
     msg['From'] = 'your_email@gmail.com'
     msg['To'] = to
 
-    
     with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
         server.starttls()
         server.login(EMAIL_USER, EMAIL_PASSWORD)
